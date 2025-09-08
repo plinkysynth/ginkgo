2d16c18e8b28cdaf75b7ccb7caa0d56c456624b3
Alex Evans <715855+mmalex@users.noreply.github.com>
2025-09-08 20:19:06 +0100

    move things

--- scripts/copy_to_web.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rsync -av --include='*/' --include='*.html' --include='*.js' --include='*.wasm' --include='*.png' --exclude='*' ./ alex@serval.mythic-beasts.com:public_html/plinky12/
-

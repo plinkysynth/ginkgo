086397a0fe544ae20a4cafd8ae68c2059175516d
Alex Evans <715855+mmalex@users.noreply.github.com>
2025-11-21 14:35:48 +0000

    start of some docs

--- src/ginkgo.cpp
+++ src/ginkgo.cpp
@@ -1202,6 +1202,12 @@ static uint32_t last_mods = 0;
 
 static void key_callback(GLFWwindow *win, int key, int scancode, int action, int mods) {
     // printf("key: %d\n", key);
+    #ifdef __WINDOWS__
+    if (mods & GLFW_MOD_CONTROL) {
+        mods &= ~GLFW_MOD_CONTROL;
+        mods |= GLFW_MOD_SUPER;
+    }
+    #endif
     last_mods = mods;
     EditorState *E = curE;
     if (action != GLFW_PRESS && action != GLFW_REPEAT)

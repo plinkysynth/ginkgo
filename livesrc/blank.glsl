b3c8d84f1919adfc890de5fc59fd713969b76335
Alex Evans <715855+mmalex@users.noreply.github.com>
2025-11-26 11:44:00 +0000

    add welcome message

--- livesrc/blank.glsl
+++ livesrc/blank.glsl
@@ -1,3 +1,28 @@
+/*
+hello and welcome to ginkgo!
+
+this is blank.glsl, an empty GLSL shader to make visuals.
+feel free to edit the code below - in the style of shadertoy - to create an image
+press Cmd+S to save & recompile, or Cmd+Enter to just recompile.
+
+Other useful shortcut keys:
+
+F1 = VISUALS (glsl + uzu)
+F2 = AUDIO (C++ + uzu)
+F3 = CANVAS (drawing!)
+F4 = Sample selector
+F12 = tap tempo. (updates a /bpm pattern in the audio tab)
+
+If you press F1-F4 repeatedly it toggles the visibility of the code on that tab.
+On F1 and F2 tabs, the part inside #ifdef PATTERNS is written in a tidal-like uzu lang.
+
+Cmd+P = play/pause
+Cmd+Comma / Cmd+Period = stop
+Cmd+[] = skip forward or backward
+Cmd+Q = quit
+
+For more information, see docs/docs.md
+*/
 #ifdef PATTERNS
 #endif
 vec4 pixel(vec2 _uv) {
